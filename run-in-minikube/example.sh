08315b4128f5ff35aabceb7655038d11d90fd6e0
mgh35 <markgeorgehiggins@hotmail.com>
2020-06-26 15:01:56 -0400

    Add ingress

--- run-in-minikube/example.sh
+++ run-in-minikube/example.sh
@@ -8,7 +8,9 @@ DIR=../cluster/
 kubectl apply -f $DIR
 
 kubectl get all
-
 kubectl run cmd-in-cluster --image=busybox --attach=true --restart=Never --rm -- wget -q -O - http://hello-world:5000
+
+INGRESS_IP=$(kubectl get ingress ingress -o json | jq '.status.loadBalancer.ingress[0].ip' -r)
+wget -q -O - http://${INGRESS_IP}/world
 
 kubectl delete -f $DIR
