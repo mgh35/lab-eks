8672e0c467c71e0fc9c71a9083c367bf633ff565
mgh35 <markgeorgehiggins@hotmail.com>
2020-06-29 15:54:45 -0400

    Tidy

--- app-hello-world/build.sh
+++ app-hello-world/build.sh
@@ -6,8 +6,8 @@ cd "$(dirname "$0")"
 read "TAG?Tag: "
 NAME="mgh35/hello-world:$TAG"
 
-printf "Building image: $NAME"
-docker build --tag $NAME .
+printf "Building image: %s" "$NAME"
+docker build --tag "$NAME" .
 
 docker login
-docker push $NAME
+docker push "$NAME"
